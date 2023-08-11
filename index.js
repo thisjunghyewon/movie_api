eeeef34f7d897ff1915a10e40a85aa90aa3b8c0e
thisjunghyewon <this.junghyewon@gmail.com>
2023-08-11 14:02:34 +0200

    Update index.js
    Replace the connetion URI

--- index.js
+++ index.js
@@ -37,7 +37,11 @@ const passport = require("passport");
 require("./passport");
 
 // This allows Mongoose to connect to that database.
-mongoose.connect("mongodb://127.0.0.1:27017/cfDB", {
+// mongoose.connect("mongodb://127.0.0.1:27017/cfDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
